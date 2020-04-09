9a7790c0aa58c895b6647af1ae5872ee4b2f8f24
Lyumih <m.ivanov1225@gmail.com>
2020-04-09 21:26:37 +0300

    feat (web): js

--- public/js/index.js
+++ public/js/index.js
@@ -1,48 +1,25 @@
 const app = new Vue({
   el: "#app",
   data: {
-    day: 1,
-    month: 1,
-    year: 1980,
-    lines: []
+    day: 2,
+    month: 4,
+    year: 1996,
   },
   computed: {
     chs() {
-      return this.calculate(12);
+      return simplify(this.day)
     },
     mission() {
-      return this.calculateMission(this.day, this.month, this.year);
+      return simplify(this.day + "" + this.month + "" + this.year)
     },
-    matrix() {
-        let numbers = this.dateString.split("")
-        return numbers.filter(number => number > 0)
+    selfYear() {
+      return simplify(this.day + "" + this.month + "" + new Date().getFullYear())
     },
-    dateString() {
-      return ("" + this.day + this.month + this.year);
+    selfMonth() {
+      return simplify(this.selfYear + " " + new Date().getMonth() + 1)
     },
-    matrixPretty() {
-        return {
-            1: this.matrix.filter(item => item == 1),
-            2: this.matrix.filter(item => item == 2),
-            3: this.matrix.filter(item => item == 3),
-            4: this.matrix.filter(item => item == 4),
-            5: this.matrix.filter(item => item == 5),
-            6: this.matrix.filter(item => item == 6),
-            7: this.matrix.filter(item => item == 7),
-            8: this.matrix.filter(item => item == 8),
-            9: this.matrix.filter(item => item == 9),
-        }
-    }
-  },
-  methods: {
-    calculate(number) {
-      let simpleNumber = 0;
-      if (number > 9) {
-      }
-      return number;
-    },
-    calculateMission(day, month, year) {
-      return this.calculate(15);
+    selfDay() {
+      return simplify(this.selfMonth + " " + new Date().getDate())
     }
   }
 });
--- script/test.js
+++
@@ -1,5 +0,0 @@
-let suisai = require('./index')
-
-console.log(suisai)
-
-console.log(suisai.simplify("12.22.1234"))
