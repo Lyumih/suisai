f5d3555172a469df29f50e7cc031ccd78b171668
Lyumih <m.ivanov1225@gmail.com>
2020-04-09 21:43:09 +0300

    feat (web): optimize

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    day: "",
+    month: "",
+    year: "",
+  },
+  computed: {
+    chs() {
+      return simplify(this.day)
+    },
+    mission() {
+      return simplify(this.day + "" + this.month + "" + this.year)
+    },
+    selfYear() {
+      return simplify(this.day + "" + this.month + "" + new Date().getFullYear())
+    },
+    selfMonth() {
+      return simplify(this.selfYear + " " + new Date().getMonth() + 1)
+    },
+    selfDay() {
+      return simplify(this.selfMonth + " " + new Date().getDate())
+    }
+  }
+});
